806e44afb1098f00c5c9da1038f7cf088fc572b4
remi-code <jhonedoe@gmail.com>
2022-08-03 23:13:32 +0530

    searchInsert

---
+++ SearchInsertPosition.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    
+    
+    public static int searchInsert(int[] nums, int target) {
+        for(int i = 0;i<nums.length;i++){
+            if(target<=nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,3,5,6};
+        int target = 5;
+        System.out.println(searchInsert(a,target));
+
+    }
+}
