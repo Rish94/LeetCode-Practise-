d569293687e2c3ab2f0c1b052440c4ead4fc4241
remi-code <jhonedoe@gmail.com>
2022-08-04 19:50:32 +0530

    smallchange

--- FindlastWordLength.java
+++ FindlastWordLength.java
@@ -3,7 +3,6 @@ public class FindlastWordLength
 {
 	public static void main(String[] args) {
 		String a = "Hello World";
-		int l = 0;
 		StringTokenizer st = new StringTokenizer(a);
 		ArrayList<Integer> arr = new ArrayList<>();
 		while(st.hasMoreTokens()){
